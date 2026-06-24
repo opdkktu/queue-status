9d9ed75dd1d7d5d8a39a90873aa8e813f5b7512a
opdkktu <opd.kktu@moh.gov.my>
2026-06-24 12:37:08 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,16 +1,30 @@
-export async function onRequest(context) {
+export default {
+
+async fetch(request, env){
+
+const url = new URL(request.url);
 
 
-const url = new URL(context.request.url);
+// Hide GAS URL here
+if(url.pathname === "/api/ayer-hitam"){
 
 
-const gasURL =
+const GAS_URL =
 "https://script.google.com/macros/s/AKfycbwNHjoocayxuLE7YfoE36R_u3M0VAvnW-qWDEF-RDXkEGZwzUjutvQeHvQHidXG_c59vQ/exec";
 
 
 return fetch(
-gasURL + url.search
+GAS_URL + url.search
 );
 
+
+}
+
+
+// everything else = your website
+return env.ASSETS.fetch(request);
+
+
+}
 
 }
