1284b70eeec1760da55740f44061c4c2e8d6503d
opdkktu <opd.kktu@moh.gov.my>
2026-06-24 12:41:05 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,30 +1,37 @@
 export default {
-
 async fetch(request, env){
-
 const url = new URL(request.url);
+// Clinic GAS mapping
+const CLINICS = {
+  "ayer-hitam":
+  "https://script.google.com/macros/s/AKfycbwNHjoocayxuLE7YfoE36R_u3M0VAvnW-qWDEF-RDXkEGZwzUjutvQeHvQHidXG_c59vQ/exec",
+  "jitra":
+  "https://script.google.com/macros/s/JITRA_GAS_URL_HERE/exec",
+  "pokok-sena":
+  "https://script.google.com/macros/s/POKOK_SENA_GAS_URL_HERE/exec"
+};
 
-
-// Hide GAS URL here
-if(url.pathname === "/api/ayer-hitam"){
-
-
-const GAS_URL =
-"https://script.google.com/macros/s/AKfycbwNHjoocayxuLE7YfoE36R_u3M0VAvnW-qWDEF-RDXkEGZwzUjutvQeHvQHidXG_c59vQ/exec";
-
-
-return fetch(
-GAS_URL + url.search
-);
-
-
+// check if request is API
+if(url.pathname.startsWith("/api/")){
+  // get clinic name after /api/
+  const clinicID =
+  url.pathname.replace("/api/","");
+  const gasURL =
+  CLINICS[clinicID];
+  // unknown clinic
+  if(!gasURL){
+    return new Response(
+      "Clinic not found",
+      {
+        status:404
+      }
+    );
+  }
+  // forward to GAS
+  return fetch(
+    gasURL + url.search
+  );
 }
-
-
-// everything else = your website
+// normal website loading
 return env.ASSETS.fetch(request);
-
-
-}
-
-}
+}}
